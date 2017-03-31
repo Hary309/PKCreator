0d4038609cd614af011551f8c1a5985763556719
Piotr Krupa <piotrkrupa06@gmail.com>
2017-03-31 22:11:51 +0200

    Added sfml renderer to event window

--- src/PK Creator/EventItemWindow.h
+++ src/PK Creator/EventItemWindow.h
@@ -1,18 +1,28 @@
 #pragma once
 
 #include <QDialog>
+#include <QTimer>
 
 class QStandardItem;
 class QCloseEvent;
 
 class EventItem;
 
+namespace sf
+{
+	class RenderWindow;
+	class RectangleShape;
+}
+
 class EventItemWindow : public QDialog
 {
 	Q_OBJECT
 
 private:
-	EventItem		*m_pItemParent;
+	sf::RenderWindow		*m_pRenderer;
+	EventItem				*m_pItemParent;
+
+	QTimer					m_timer;
 
 private:
 	void closeEvent(QCloseEvent *e) override;
@@ -22,5 +32,7 @@ public:
 	~EventItemWindow();
 
 	void FillData(EventItem *item);
+
+	void Render();
 };
 
